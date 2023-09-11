70d2cda2fc87c16b5226bbc028662a74977ba85a
Kisjolanka01 <lytwynd08@gmail.com>
2023-09-11 11:36:16 +0200

    09.11

---
+++ elso.py
@@ -0,0 +1,10 @@
+nemjo = True
+while nemjo:
+    szam_str= input('szám:')
+    if szam_str.isdecimal():
+        nemjo=False
+        szam=int(szam_str)
+if szam >3:
+    print("nagyobb")
+elif szam <3:
+    print("kisebb")
