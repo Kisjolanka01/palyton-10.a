440a984559f286636c091a3b929e44530219e409
Kisjolanka01 <lytwynd08@gmail.com>
2023-10-02 11:43:01 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Kókuszgoló.py
+++ Kókuszgoló.py
@@ -1,9 +1,14 @@
+import random
+eladasok = 0
+sin = 1
+
+
 def feltoltes_adatszerkezet():
     for i in range(10):
         eladasok[i] = 1
 
 def osszes_eladas():
-    return sum(eladsok)
+    return sum(eladasok)
 
 
 def heti_eladas(het):
@@ -19,5 +24,8 @@ def legkevesebb_paratlan_eladas_nap():
     min_paratlan = min(eladasok[1::2])
     return eladasok.index(min_paratlan)
 
-
- 
+print("osszes eladas",osszes_eladas())
+print("heti eladas(4.heti):", heti_eladas(4))
+print("legsikeresebb het", legsikeresebb_het())
+print("eladas mentes napok:", eladasmentes_napok())
+print(" legkevesebb paratlan eladas nap", legkevesebb_paratlan_eladas_nap())
