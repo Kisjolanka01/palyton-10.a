3f39beedfbc38b21b2071a7cba6ff680e407c79c
Kisjolanka01 <lytwynd08@gmail.com>
2023-10-23 11:06:47 +0200

    Hazi

--- Házi.py
+++ Házi.py
@@ -1,4 +1,4 @@
-def szigoruan_monoton_novekvo(szamok):
+'''def szigoruan_monoton_novekvo(szamok):
     n = len(szamok)
     for i in range(1, n):
         if szamok[i] <= szamok[i - 1]:
@@ -9,4 +9,33 @@ szamlista = [1, 10, 784, 63, 309]
 if szigoruan_monoton_novekvo(szamlista):
     print("A számlista szigorúan monoton növekvő")
 else:
-    print("A számlista nem szigorúan monoton növekvő")
+    print("A számlista nem szigorúan monoton növekvő")'''
+
+
+
+def atvaltas_tizes_szamrendszerbe(szam, forras_szamrendszer):
+    tizes_szamrendszerben = 0
+    for i, jegy in enumerate(reversed(szam)):
+        tizes_szamrendszerben += int(jegy) * (forras_szamrendszer ** i)
+    return tizes_szamrendszerben
+
+def atvaltas_mas_szamrendszerbe(tizes_szamrendszerben, cel_szamrendszer):
+    eredmeny = ''
+    while tizes_szamrendszerben > 0:
+        maradek = tizes_szamrendszerben % cel_szamrendszer
+        eredmeny = str(maradek) + eredmeny
+        tizes_szamrendszerben //= cel_szamrendszer
+    return eredmeny
+
+def main():
+    forras_szamrendszer = int(input("Kérem a forrás számrendszer alapszámát (pl. 2, 8, 10 stb.): "))
+    cel_szamrendszer = int(input("Kérem a cél számrendszer alapszámát (pl. 2, 8, 10 stb.): "))
+    szam = input("Kérem a konvertálni kívánt számot: ")
+
+    tizes_szamrendszerben = atvaltas_tizes_szamrendszerbe(szam, forras_szamrendszer)
+    eredmeny = atvaltas_mas_szamrendszerbe(tizes_szamrendszerben, cel_szamrendszer)
+
+    print(f"A(z) {szam} szám {forras_szamrendszer} számrendszerből átkonvertálva {cel_szamrendszer} számrendszerbe: {eredmeny}")
+
+if __name__ == "__main__":
+    main()
