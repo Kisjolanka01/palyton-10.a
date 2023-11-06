6f067c153177844aaa3c2e5e25e9fd9a93524127
Kisjolanka01 <lytwynd08@gmail.com>
2023-11-06 11:45:51 +0100

    11.06

---
+++ 11_06.py
@@ -0,0 +1,17 @@
+x=print("Hello")
+print(x)
+
+def fv(num):
+    if num>5:
+        return 0
+    else:
+        print("kisebb")
+
+y=fv(3)
+print(y)
+print("hello\nWo\trld")
+print(len("\n"))
+
+print("yoyoyo húsnap\\number")
+print("elsoszo", "masodikszo", end=" ")
+print("Python")
