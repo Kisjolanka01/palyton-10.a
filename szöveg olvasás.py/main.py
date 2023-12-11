3c3e1289cc5348bbce44e675f441370f74dec9ca
Kisjolanka01 <lytwynd08@gmail.com>
2023-12-11 11:46:26 +0100

    a

--- szöveg olvasás.py/main.py
+++ szöveg olvasás.py/main.py
@@ -1,14 +1,33 @@
-fajlnev="input.txt"
-f=open(fajlnev, "r")
-sorok=f.readlines()
-f.close()
-for i in range(len(sorok)):
-    sorok[i]=sorok[i].strip()
-print(sorok[0])
-sum=int(sorok[1])+int(sorok[2])
-print(sum)
+# Adatok:
+#   Fájlban:    Dolgozók nevei szerepelnek
+#               Mellette hogy havonta mennyi palacsintát sütöttek meg
 
+# 1) Olvasd be a fájlt megfelelő adatszerkezetbe!
+filename="input.txt"
+file=open(filename,"r")
+file_lista=file.readlines()
+file.close()
 
+print(file_lista)
+name_list=[]
+pancake_list=[]
+for i in range(len(file_lista)):
+    name_list.append(lista_parts[0])
 
+    lista_parts=file_lista[i].strip().split()
 
+    _l=[]
+    for j in range(1,len(lista_parts)):
+        _l.append(int(lista_parts[j]))
+    pancake_list.append(_l)
 
+print(name_list)
+print(pancake_list)
+for i in range(len(pancake_list)):
+    print(len(pancake_list[i]))
+
+#print(file_lista)
+# 2) írd ki az adatokat szépen(!!) az alábbbi fejléccel:
+#       "Név\tJanuary\tFebruary\tMarch\tApril\tMay\tJune\tJuly\tAugust\tSeptember\tOctober\tNovember\tDecember"
+fejlec="Név\tJanuary\tFebruary\tMarch\tApril\tMay\tJune\tJuly\tAugust\tSeptember\tOctober\tNovember\tDecember"
+print(fejlec)
