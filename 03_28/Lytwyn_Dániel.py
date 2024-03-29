20b6457a8ddd8c02212b5a6a6aa45dab8e0e906e
Kisjolanka01 <lytwynd08@gmail.com>
2024-03-29 12:34:31 +0100

    a

--- 03_28/Lytwyn_Dániel.py
+++ 03_28/Lytwyn_Dániel.py
@@ -1,5 +1,3 @@
-# 1. feladat
-
 file = open("adat.txt", "r", encoding="UTF-8")
 lista = []
 for sor in file:
@@ -7,31 +5,23 @@ for sor in file:
 file.close()
 
 for i in range(len(lista)):
-    for j in range(len(lista[i])):
-        if j == 0:
-            lista[i][j] = int(lista[i][j])
-        if j == 4:
+    for j in range(0, len(lista[i]), 4):
             lista[i][j] = int(lista[i][j])
 
-# 2. feladat
-eladasok = 0
-for i in range(len(lista)):
-    eladasok += 1
-print(f"2. feladat\nA fájlban található tranzakciók (eladások) száma: {eladasok}")
+print("2. feladat")
+print(f"A fájlban található tranzakciók (eladások) száma: {len(lista)}")
 
-# 3. feladat
 darab = 0
 for i in range(len(lista)):
-    for j in range(len(lista)):
-        if j == 4:
-            darab += lista[i][j]
-print(f"3. feladat\nÖsszes eladott termék száma: {darab} db")
+    for j in range(4, len(lista[i]), 4):
+        darab += lista[i][j]
+print("3. feladat")
+print(f"Összes eladott termék száma: {darab} db")
 
-# 4. feladat
 print("4. feladat")
 while True:
     nap = int(input("Add meg a nap sorszámát [1...28]: "))
-    if nap > 0 and nap < 29:
+    if 1 <= nap <= 28:
         break
     print("Hibás adatbevitel! Próbáld újra...")
 
@@ -45,22 +35,21 @@ for i in range(len(lista)):
 if forgalom == False:
     print("A megadott napon nem volt forgalma a cégnek.")
 
-# 5. feladat
 termekek = [600, 750, 550, 650, 450]
 maximumertek = 0
-legtobb_erteku_munkas =  ""
-print("5. feladat\nAz egyes munkatársak által generált bevételek:")
+maxertekmunkas = ""
+print("5. feladat")
+print("Az egyes munkatársak által generált bevételek:")
 for i in range(1, 6):
     munkasbevetel = 0
-    munkas =  ""
+    munkaskod = "M"+str(i)
     for j in range(len(lista)):
         for k in range(len(lista[j])):
-            if lista[j][k] == f"M{i}":
+            if lista[j][k] == munkaskod:
                 munkasbevetel += lista[j][k - 3] * termekek[int(lista[j][k + 1].replace("T", "")) - 1]
-                munkas = f"M{i}"
-    if munkasbevetel > maxertek:
-        maxertek = munkasbevetel
-        legtobb_erteku_munkas = munkas
+                munkas = munkaskod
+    if munkasbevetel > maximumertek:
+        maximumertek = munkasbevetel
+        maxertekmunkas = munkas
     print(f"{munkas}: {munkasbevetel} Ft")
-print(f"A legtöbb bevételt az {legtobb_erteku_munkas} kódú munkatárs generálta.")
-# 6. feladat
+print(f"A legtöbb bevételt az {maxertekmunkas} kódú munkatárs generálta.")
